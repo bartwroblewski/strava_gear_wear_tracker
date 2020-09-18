bf807604ec28470c28150432dcd5eb3370938752
bartwroblewski <barti.wroblewski@gmail.com>
2020-09-18 20:05:58 +0200

    Unique gear per user

--- tracker/models.py
+++ tracker/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+def unique_gear_name_validator(gear_name):
+    pass
+
 # Create your models here.
 class Gear(models.Model):
     name = models.CharField(max_length=200)
-    mileage = models.FloatField(default=0)#(decimal_places=2, max_digits=8)
+    mileage = models.FloatField(default=0)
     is_tracked = models.BooleanField(default=True)
     user = models.ForeignKey(
         get_user_model(),
@@ -14,3 +17,6 @@ class Gear(models.Model):
 
     def __str__(self):
         return f'{self.name}, user: {self.user}, is tracked: {self.is_tracked}'
+
+    class Meta:
+        unique_together = ('name', 'user')
