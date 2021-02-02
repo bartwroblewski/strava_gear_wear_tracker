adf61d84222ae8787002851bbed2a23e0eeca324
bartwroblewski <barti.wroblewski@gmail.com>
2021-02-02 14:16:53 +0100

    static

--- weartracker/settings.py
+++ weartracker/settings.py
@@ -28,7 +28,7 @@ CLIENT_ID = os.environ['STRAVA_CLIENT_ID']
 CLIENT_SECRET = os.environ['STRAVA_CLIENT_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DOMAIN = 'stravageartracker.herokuapp.com'#'localhost'#'f918bae6f5b0.ngrok.io'#
 ALLOWED_HOSTS = [DOMAIN]
